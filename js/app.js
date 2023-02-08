57e999dd23e32be117e15c9f2f2d0916598031d2
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-08 17:53:56 +0100

    FEAT pari e dispari

--- js/app.js
+++ js/app.js
@@ -4,34 +4,73 @@ console.log('ciao')
 // Chiedere all’utente di inserire una parola
 
 // Creare una funzione per capire se la parola inserita è palindroma
-const string = prompt('Dimmi una parola e vediamo se è palindroma: ');
-const value = isPalindroma(string) 
+// const string = prompt('Dimmi una parola e vediamo se è palindroma: ');
+// const value = isPalindroma(string) 
 
 
-function isPalindroma(str) {   
-    const lunghezzaParola = str.length; 
-    console.log(string) 
-    // uso un loop per dividere le parole in due parti e poi le confronto 
-    for (let i = 0; i < lunghezzaParola / 2; i++) {  
-        // se la prima parte divisa è uguale alla seconda allora sono palindrome sennò non lo è 
-        if (str[i] !== str[lunghezzaParola - 1 - i]) {  
-          console.log('non è palindromo')
-          return;
-        }  
-    }  
-    console.log( 'è palindroma'); 
+// function isPalindroma(str) {   
+//     const lunghezzaParola = str.length; 
+//     console.log(string) 
+//     // uso un loop per dividere le parole in due parti e poi le confronto 
+//     for (let i = 0; i < lunghezzaParola / 2; i++) {  
+//         // se la prima parte divisa è uguale alla seconda allora sono palindrome sennò non lo è 
+//         if (str[i] !== str[lunghezzaParola - 1 - i]) {  
+//           console.log('non è palindromo')
+//           return;
+//         }  
+//     }  
+//     console.log( 'è palindroma'); 
      
-}  
+// } 
 
-    
-
-
-// Comunicare all’utente (decidete voi come) se la parola che ha inserito è palindroma oppure no
 
 
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
+
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Comunichiamo all’utente chi ha vinto. (decidete voi come)
+    const askParioDispari = prompt('Dimmi pari o dispari')
+    const numberUser = parseInt(prompt('Dimmi anche un numero da 1 a 5'))
+    console.log(askParioDispari)
+    console.log('numero user', numberUser)
+ // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+    const numberPc = parseInt(Math.random() * 5 + 1 )
+    console.log('numero pc', numberPc)
+    const sommaNumeri = numberPc + numberUser
+// Sommiamo i due numeri
+    const resto = sommaNumeri % 2
+
+if ((isEven(sommaNumeri)) && (askParioDispari === 'pari')) {
+    document.getElementById('messaggio').innerHTML = 'la somma è pari ' + sommaNumeri + ' hai vinto'
+	console.log('la somma è pari', sommaNumeri, ' hai vinto')
+} else{
+    document.getElementById('messaggio').innerHTML = 'Mi spiace hai perso. La somma è ' + sommaNumeri
+    console.log('Mi spiace hai perso. La somma è ', sommaNumeri)
+}
+
+if ((isOdd(sommaNumeri)) && (askParioDispari === 'dispari')) {
+    document.getElementById('messaggio').innerHTML = 'la somma è dispari ' + sommaNumeri + ' hai vinto'
+	console.log('la somma è dispari', sommaNumeri)
+} 
+
+
+
+// isEven(numero) //true se numero è pari, false se il numero è dispari
+// isOdd(numero) //true se il numero è dispari, false se il numero è pari
+
+function isEven(num) {
+	const resto = num % 2
+	if (resto === 0) {
+		return true
+	} else {
+		return false
+	}
+}
+
+function isOdd(_num) {
+	if(resto === 1) {
+	  return true
+	} else {
+	  return false
+	}
+}
